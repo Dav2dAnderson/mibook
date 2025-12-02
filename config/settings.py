0e852d71fcd4a81d7a5b895a8476d68fdab0b906
Dav2dAnderson <dav2danderson@gmail.com>
2025-12-02 20:33:31 +0500

    fix: fixed some mistakes in actions file

--- config/settings.py
+++ config/settings.py
@@ -151,13 +151,13 @@ MEDIA_ROOT = BASE_DIR / 'media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = env("EMAIL_HOST")
-EMAIL_PORT = env("EMAIL_PORT")
-EMAIL_USE_TLS  = env.bool("EMAIL_USE_TLS")
+EMAIL_BACKEND = env("EMAIL_BACKEND", default='django.core.mail.backends.locmem.EmailBackend')
+EMAIL_HOST = env("EMAIL_HOST", default="localhost")
+EMAIL_PORT = env("EMAIL_PORT", default=1025)
+EMAIL_USE_TLS  = env.bool("EMAIL_USE_TLS", default=False)
 
-EMAIL_HOST_USER = env("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER", default="")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD", default="")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
